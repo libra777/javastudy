b10c47a39a6579ee6b672947a82664508a26abbe
qipengsun <qipeng.sun@gmail.com>
2012-10-08 11:26:42 +0800

    modify the finaly name

---
+++ app/src/main/java/edu/sun/app/framework/mbean/security/AppRealm.java
@@ -0,0 +1,28 @@
+package edu.sun.app.framework.mbean.security;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * User: sunqipeng
+ * Date: 12-10-8
+ */
+public class AppRealm implements Realm {
+    public String getName() {
+        return "apprealm";
+    }
+
+    public boolean supports(AuthenticationToken token) {
+        if (token instanceof UsernamePasswordToken) {
+
+        }
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
