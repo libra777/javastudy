aaa0a496e10b483132cd880126c2e316ad408de3
qipengsun <qipeng.sun@gmail.com>
2012-10-08 23:45:49 +0800

    modify the login page

--- app/src/main/java/edu/sun/app/framework/security/AppRealm.java
+++ app/src/main/java/edu/sun/app/framework/security/AppRealm.java
@@ -8,12 +8,16 @@ import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * User: sunqipeng
  * Date: 12-10-8
  */
 public class AppRealm extends AuthorizingRealm {
+    static Logger logger = LoggerFactory.getLogger(AppRealm.class);
+
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
         System.out.println("the shiro invoke the realm application");
@@ -24,6 +28,9 @@ public class AppRealm extends AuthorizingRealm {
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         System.out.println("the shiro invoke the realm application");
+        logger.info(token.getPrincipal().toString());
+        logger.info(token.getCredentials().toString());
+        logger.info(token.toString());
         return new SimpleAuthenticationInfo(new LogInUser(), token.getCredentials(), this.getName());
     }
 }
